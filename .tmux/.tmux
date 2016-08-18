4b4fa541da536a9ba3b65275c0c8084dcff0533a
Donald Perry <dj@stti.org>
2016-08-18 11:02:51 -0400

    Changes to run installs on each os... sure will use it again, and to start adding some color

---
+++ .tmux/.tmux
@@ -0,0 +1,1 @@
+/Users/donaldperry/dotfiles/.tmux
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+os=$(uname -s)
+echo "${green}Bootstraping for"
+echo ${red}$os
+echo "${green}Installing packages${reset}"
+if [[ $os == "Linux" ]]; then
+    sudo apt-get -y install tmux
+    sudo apt-get -y install zsh
+fi
+
+if [[ $os == "Darwin" ]]; then
+    brew install tmux
+    brew install zsh
+fi
+
+#tmux setup
+echo "${green}Setting up tmux${reset}"
 ln -s ~/dotfiles/.tmux ~/.tmux
 ln -s ~/dotfiles/.tmux/.tmux.conf ~/.tmux.conf
 ~/.tmux/plugins/tpm/bin/install_plugins
